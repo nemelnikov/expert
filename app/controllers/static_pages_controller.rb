b7817f8ec0f69bfa87ef40ed52c3f7a3ac1a4aca
nemelnikov <melnikov.n.e@gmail.com>
2015-05-17 13:45:09 +0400

    add 2 static pages and bootstrap menu

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  
+  def topics
+  end
+
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  root 'static_pages#home'
+  get 'index' => 'static_pages#index', as: 'index'
+  get 'help' => 'static_pages#help', as: 'help'
+  get 'genres' => 'static_pages#genres', as: 'genres'
+  root 'static_pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
