ea04098c93a06ee9cb72a218246ac6265d49a938
VISHAL DEEPAK <vishal.deepak@outlook.com>
2015-06-02 16:30:02 +0530

    Made connections between expert login and expert Profile

--- app/models/ask_expert.rb
+++ app/models/ask_expert.rb
@@ -4,6 +4,7 @@ class AskExpert < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  
+  has_one :profile_expert,  dependent: :destroy
+  after_create :build_profile_expert  
 
 end
